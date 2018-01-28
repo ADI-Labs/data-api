470e903936a1fdce7f7b26b796178a7e846e8ad0
Jonathan Zhang <jz2814@columbia.edu>
2018-01-28 12:14:53 -0500

    added travis

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
-from flask import Flask, render_template
+# from click import echo
+from flask import Flask, render_template, current_app
+# from unittest import TestLoader, TextTestRunner
+# from tests.test_basics_app import BasicsTest
 
 app = Flask(__name__)
+
+
+# @app.cli.command()
+# def test():
+#     echo("Testing...")
+#     suite = TestLoader().loadTestsFromTestCase(BasicsTest)
+#     TextTestRunner().run(suite)
 
 
 @app.route('/')
