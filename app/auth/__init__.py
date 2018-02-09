365b4f58ae68dc429a9f5c0139c6da6c1232ed1e
James Xu <jrx2000@columbia.edu>
2018-02-09 17:02:24 -0500

    updated forms

--- app/auth/__init__.py
+++ app/auth/__init__.py
@@ -1,5 +1,16 @@
 from flask import Blueprint
+from . import views  # noqa: F401
+from flask.ext.login import LoginManager
 
 auth = Blueprint('auth', __name__)
 
-from . import views  # noqa: F401
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
+
+def create_app(config.app):
+	from .auth import auth as auth_blueprint
+	app.register_blueprint(auth_blueprint, url_prefix='/auth')
+	login_manager.init_app(app)
+	return app
+
