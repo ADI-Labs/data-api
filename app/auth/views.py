bcb849d06f2bf0845e6f97cff391f2d51e0770f8
James Xu <jrx2000@columbia.edu>
2018-02-09 17:01:38 -0500

    worked on initial forms

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,33 +1,7 @@
-from flask_login import login_user, login_required, logout_user
-from flask import redirect, abort, request, Response
-from ..models import User
+from flask import render_template
 from . import auth
 
-
-@auth.route("/login", methods=['GET', 'POST'])
+@auth.route('/login')
 def login():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        if password == username + "_secret":
-            id = username.split('user')[0]
-            user = User(id)
-            login_user(user)
-            return redirect(request.args.get("next"))
-        else:
-            return abort(401)
-    else:
-        return Response('''
-        <form action="" method="post">
-            <p><input type=text name=username>
-            <p><input type=password name=password>
-            <p><input type=submit value=Login>
-        </form>
-        ''')
+		return render_template('auth/login.html')
 
-
-@auth.route('/logout')
-@login_required
-def logout():
-    logout_user()
-    return Response('<p>Logged out</p>')
---
+++ app/auth/views_old.py
@@ -0,0 +1,37 @@
+from flask_login import login_user, login_required, logout_user
+from flask import redirect, abort, request, Response
+from ..models import User
+from . import auth
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+
+
+@auth.route("/login", methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        if password == username + "_secret":
+            id = username.split('user')[0]
+            user = User(id)
+            login_user(user)
+            return redirect(request.args.get("next"))
+        else:
+            return abort(401)
+    else:
+        return Response('''
+        <form action="" method="post">
+            <p><input type=text name=username>
+            <p><input type=password name=password>
+            <p><input type=submit value=Login>
+        </form>
+        ''')
+
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return Response('<p>Logged out</p>')
