f72387c5815e54896e4274bef439331574a53da0
amqzong <AmandaZong@MacBook-Pro.local>
2018-02-09 14:54:31 -0500

    Response to failed test

--- app/main/courses.py
+++ app/main/courses.py
@@ -1,6 +1,5 @@
-from flask import Flask, render_template, Response, redirect, url_for, request, session, abort
+from flask import Flask
 from flask_restful import Resource, Api
-from flask_login import LoginManager, UserMixin, login_required, login_user, logout_user
 from . import main
 
 class Courses(Resource):
