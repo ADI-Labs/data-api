c43722b70037fa8598a2c421f5a998133c28e907
James Xu <jrx2000@columbia.edu>
2018-02-09 17:24:26 -0500

    updated venv

--- app/auth/__init__.py
+++ app/auth/__init__.py
@@ -1,16 +1,7 @@
 from flask import Blueprint
-from . import views  # noqa: F401
-from flask.ext.login import LoginManager
 
 auth = Blueprint('auth', __name__)
 
-login_manager = LoginManager()
-login_manager.session_protection = 'strong'
-login_manager.login_view = 'auth.login'
+from . import views  # noqa: F401
 
-def create_app(config.app):
-	from .auth import auth as auth_blueprint
-	app.register_blueprint(auth_blueprint, url_prefix='/auth')
-	login_manager.init_app(app)
-	return app
 
