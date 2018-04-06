d591113c2451a30b7cd8b43f50e489293c5c336c
KathyLau <kathy@mit.edu>
2018-04-06 14:03:50 -0400

    add classes for housing and dining

--- app/api/api.py
+++ app/api/api.py
@@ -1,7 +1,7 @@
 from flask import jsonify
 from flask_restful import Api, Resource, abort
 from .. import db
-from ..models import Course, User
+from ..models import Course, User, Dining
 from . import api_bp
 
 api = Api(api_bp)
@@ -27,6 +27,38 @@ class Courses(Resource):
         return jsonify(datum)
 
     """we are not going to have sets and deletes"""
+
+
+class Dining(Resource):
+    def get(self, term, key):
+        if User.verify(key) == None:
+            return "invalid api key"
+
+        result = Dining.query.filter_by(term=term).first()
+        datum = {}
+
+        if result is None:
+            abort(404, status=400, message=f'Dining for term {term} does not exist')
+
+        datum['status'] = 200
+        datum['data'] = [remove_hidden_attr(result.__dict__)]
+        return jsonify(datum)
+
+
+class Housing(Resource):
+    def get(self, term, key):
+        if User.verify(key) == None:
+            return "invalid api key"
+
+        result = Housing.query.filter_by(term=term).first()
+        datum = {}
+
+        if result is None:
+            abort(404, status=400, message=f'Housing for term {term} does not exist')
+
+        datum['status'] = 200
+        datum['data'] = [remove_hidden_attr(result.__dict__)]
+        return jsonify(datum)
 
 
 api.add_resource(Courses, '/courses/<term>/<cid>/<key>')
