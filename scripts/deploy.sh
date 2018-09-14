18e21eece1d2fe68e228d50fa0fceb63e7e82cd6
Jonathan Zhang <jz2814@columbia.edu>
2018-09-14 14:52:02 -0400

    oops

--- scripts/deploy.sh
+++
@@ -1,9 +0,0 @@
-read -p 'uni: ' uni
-read -sp 'password: ' password
-
-echo '{"login":{"uni": $uni, "password": $password}}' > config.json
-
-git pull origin master
-git remote add dokku dokku@208.68.37.106:data-api
-git push -f dokku master
-git remote rm dokku
