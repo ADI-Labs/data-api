43e0b3f6c316215a267e67634c80ffb7c2b490f7
Anavi Lohia <anavi@dyn-209-2-225-196.dyn.columbia.edu>
2018-10-05 16:46:57 -0400

    removed random spaces

--- app/docs.py
+++ app/docs.py
@@ -4,124 +4,122 @@ class QueryParameter:
         self.par_type = par_type
         self.description = description
     
-    
     def getCourseParameters():
-        
         result = []
 
         result.append(QueryParameter(
-            'course_id', 
-            'String', 
+            'course_id',
+            'String',
             'I am a valid description'
             ))
         result.append(QueryParameter(
-            'term', 
-            'String', 
+            'term',
+            'String',
             'I am a valid description'
             ))
         result.append(QueryParameter(
-            'call_number', 
-            'Integer', 
+            'call_number',
+            'Integer',
             'I am a valid description'
             ))
         result.append(QueryParameter(
-            'course_name', 
-            'String', 
+            'course_name',
+            'String',
             'I am a valid description'
             ))
         result.append(QueryParameter(
-            'bulletin_flags', 
-            'String', 
+            'bulletin_flags',
+            'String',
             'I am a valid description'
             ))
         result.append(QueryParameter(
-            'division_code', 
-            'String', 
+            'division_code',
+            'String',
             'I am a valid description'
             ))
         result.append(QueryParameter(
-            'credit_amount', 
-            'Integer', 
+            'credit_amount',
+            'Integer',
             'I am a valid description'
             ))
         result.append(QueryParameter(
-            'prefix_name', 
-            'String', 
+            'prefix_name',
+            'String',
             'I am a valid description'
             ))
         result.append(QueryParameter(
-            'prefix_long_name', 
-            'String', 
+            'prefix_long_name',
+            'String',
             'I am a valid description'
             ))
         result.append(QueryParameter(
-            'instructor_name', 
-            'String', 
+            'instructor_name',
+            'String',
             'I am a valid description'
             ))
         result.append(QueryParameter(
-            'approval', 
-            'String', 
+            'approval',
+            'String',
             'I am a valid description'
             ))
         result.append(QueryParameter(
-            'school_code', 
-            'String', 
+            'school_code',
+            'String',
             'I am a valid description'
             ))
         result.append(QueryParameter(
-            'school_name', 
-            'String', 
+            'school_name',
+            'String',
             'I am a valid description'
             ))
         result.append(QueryParameter(
-            'campus_code', 
-            'String', 
+            'campus_code',
+            'String',
             'I am a valid description'
             ))
         result.append(QueryParameter(
-            'campus_name', 
-            'String', 
+            'campus_name',
+            'String',
             'I am a valid description'
             ))
         result.append(QueryParameter(
-            'type_code', 
-            'String', 
+            'type_code',
+            'String',
             'I am a valid description'
             ))
         result.append(QueryParameter(
-            'type_name', 
-            'String', 
+            'type_name',
+            'String',
             'I am a valid description'
             ))
         result.append(QueryParameter(
-            'num_enrolled', 
-            'Integer', 
+            'num_enrolled',
+            'Integer',
             'I am a valid description'
             ))
         result.append(QueryParameter(
-            'max_size', 
-            'Integer', 
+            'max_size',
+            'Integer',
             'I am a valid description'
             ))
         result.append(QueryParameter(
-            'min_units', 
-            'Integer', 
+            'min_units',
+            'Integer',
             'I am a valid description'
             ))
         result.append(QueryParameter(
-            'num_fixed_units', 
-            'Integer', 
+            'num_fixed_units',
+            'Integer',
             'I am a valid description'
             ))
         result.append(QueryParameter(
-            'class_notes', 
-            'String', 
+            'class_notes',
+            'String',
             'I am a valid description'
             ))
         result.append(QueryParameter(
-            'meeting_times', 
-            'String', 
+            'meeting_times',
+            'String',
             'I am a valid description'
             ))        
 
