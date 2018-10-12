79ba016e3090b81020c584c2252f52fc3d517c23
Kevin <km3290@columbia.edu>
2018-10-12 14:07:54 -0400

    recloned

--- app/api/api.py
+++ app/api/api.py
@@ -4,8 +4,6 @@ from ..models import Course, User
 from . import api_bp
 
 api = Api(api_bp)
-parser = reqparse.RequestParser()
-parser.add_argument("key")
 
 
 def remove_hidden_attr(d):
@@ -13,23 +11,151 @@ def remove_hidden_attr(d):
 
 
 class Courses(Resource):
-    def get(self, cid, term):
-        key = parser.parse_args()["key"]
-        datum = {}
-        if User.verify(key):
-            result = Course.query.filter_by(course_id=cid, term=term).first()
+    def process_args(args):
+        final_args = {}
+        # parameters = [
+        # 'course_id',
+        # 'term',
+        # 'course_name',
+        # 'call_number',
+        # 'bulletin_flags',
+        # 'division_code',
+        # 'credit_amount',
+        # 'prefix_name',
+        # 'prefix_long_name',
+        # 'instructor_name',
+        # 'approval',
+        # 'school_code',
+        # 'school_name',
+        # 'campus_code',
+        # 'campus_name',
+        # 'type_code',
+        # 'type_name',
+        # 'num_enrolled',
+        # 'max_size',
+        # 'min_units',
+        # 'num_fixed_units',
+        # 'class_notes',
+        # 'meeting_times']
+        del args['key']
 
-            if result is None:
-                abort(404, status=400,
-                      message=f'Course {cid} for term {term} does not exist')
+        for k in args.keys():
+            # if k not in parameters:
+            #     return k
+            # else:
+                # if user did not pass a value for the parameter then it will
+                # show up in args as None this confuses the sqllite db because
+                # it will look for None in db, so remove it
+            if args[k] is not None:
+                final_args[k] = args[k]
 
-            datum['status'] = 200
-            datum['data'] = [remove_hidden_attr(result.__dict__)]
+        return final_args
+
+    def get(self, typ):
+        # print(dir(Resource))
+        # adding parameters to api
+
+        # select api requires the primary keys: course id and term, and api key
+        if typ == 'select':
+            parser = reqparse.RequestParser()
+            # note that an error in passing in arguments here will not result
+            # in 400 error
+            parser.add_argument(
+                'course_id',
+                required=True,
+                help="course_id cannot be blank for select api")
+            parser.add_argument("term", required=True,
+                                help="term cannot be blank for select api !")
+            parser.add_argument("key", required=True,
+                                help="key cannot be blank!")
+
+        # search api can accept any number and combination of prameters
+        # requires api key
+        elif typ == 'search':
+            parser = reqparse.RequestParser()
+            parser.add_argument('course_id')
+            parser.add_argument("term")
+            parser.add_argument("course_name")
+            parser.add_argument("call_number")
+            parser.add_argument("bulletin_flags",)
+            parser.add_argument('division_code')
+            parser.add_argument("credit_amount")
+            parser.add_argument("prefix_name",)
+            parser.add_argument('prefix_long_name')
+            parser.add_argument('instructor_name')
+            parser.add_argument('approval')
+            parser.add_argument('school_code')
+            parser.add_argument('school_name')
+            parser.add_argument('campus_code')
+            parser.add_argument('campus_name')
+            parser.add_argument('type_code')
+            parser.add_argument('type_name')
+            parser.add_argument('num_enrolled')
+            parser.add_argument('max_size')
+            parser.add_argument('min_units')
+            parser.add_argument('num_fixed_units')
+            parser.add_argument('class_notes')
+            parser.add_argument('meeting_times')
+            parser.add_argument("key", required=True,
+                                help="key cannot be blank!")
         else:
-            datum['status'] = 400
-            datum['error'] = "User couldn't be verified"
-            datum['data'] = {}
-        return jsonify(datum)
+            abort(
+                400,
+                status=400,
+                message=f"Bad Request. GET api/courses/select?" +
+                "or api/courses/search?")
+
+        args = parser.parse_args()
+        key = args["key"]
+        datum = {}
+
+        # verify the API key
+        if User.verify(key):
+            # will return processed arguments, if there is an incorrect
+            # argument then it will return the first incorrect argument
+            args = Courses.process_args(args)
+            if isinstance(args, str):
+                abort(
+                    400,
+                    status=400,
+                    message=f"Invalid parameter: {args}")
+
+            elif isinstance(args, dict):
+                if typ == 'select':
+                    # if select api, then use get method by passing in primary
+                    # keys
+                    result = Course.query.get(
+                        (args['course_id'], args['term']))
+                    if not result:
+                        abort(
+                            404,
+                            status=404,
+                            message="Course with course_id = " +
+                            f"{args['course_id']} and term = " +
+                            "{args['term']} does not exist")
+                    else:
+                        datum['status'] = 200
+                        datum['data'] = remove_hidden_attr(result.__dict__)
+
+                elif typ == 'search':
+                    # for seach api, then use filter through db
+                    print(args)
+                    result = Course.query.filter_by(**args).all()
+                    if not result:
+                        abort(404, status=404,
+                              message=f'No courses with parameters {args}')
+
+                    else:
+                        datum['data'] = [
+                            remove_hidden_attr(
+                                i.__dict__) for i in result]
+                return jsonify(datum)
+            # if API key was not verified
+        else:
+            abort(403, status=403,
+                  message="User couldn't be verified")
 
 
-api.add_resource(Courses, '/courses/<term>/<cid>/')
+# api/courses/search dynamically searches by whatever parameters they input
+# api/courses/term/course_id passes a specific course
+api.add_resource(Courses, '/courses/<typ>') 
