b625385adea9b83c19f1575495658a8524eb1d29
Kevin <km3290@columbia.edu>
2018-10-26 13:42:04 -0400

    began adding update to helpers.py

--- app/helpers.py
+++ app/helpers.py
@@ -55,28 +55,40 @@ def get_courses():
     name = sha.hexdigest()
 
     filepath = os.path.join(FILES_STORE, "full", name)
-    if os.path.isfile(filepath):
-        print("course data is downloaded")
+    if os.path.isfile('app/data.sqlite') and os.path.isfile(filepath):
+        print("course data is downloaded and DB has data")
         print("updating...")
-        update(filepath)
+        parse_and_store(filepath)
     else:
         print("scraping and storing course data...")
-        db.drop_all()
+        # db.drop_all()
         db.create_all()
 
         crwl = CrawlerProcess(
             {'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)'})
-
         # Downloads data from courses
         crwl.crawl(CourseSpider)
         crwl.start()
 
-        parse_and_store(os.path.join(FILES_STORE, "full", name))
-        print("database created!")
+        parse_and_store(filepath)
 
-def update(path):
+    # rename to keep old files     
+    os.rename(filepath,os.path.join(FILES_STORE, "full","test.json"))
+
+
+def check_differences(existing_course, new_course):
+    pass
+
+
+def parse_and_store(path):
+    '''
+    Needs to run within an app_context!
+
+    :param path:
+    :return:
+    '''
     data = json.load(open(path))
-    for datum in data: 
+    for datum in data:
         new_course = Course(course_id=datum["Course"],
                         call_number=datum["CallNumber"],
                         course_name=datum["CourseTitle"],
@@ -99,64 +111,13 @@ def update(path):
                         type_name=datum["TypeName"],
                         type_code=datum["TypeCode"]
                         )
-        print(new_course)
-        # filter by name and course id, and take the most recent one 
-        old_courses = Course.query.filter_by(course_id = new_course.course_id, course_name = new_course.course_name).all()
-        print(old_courses)
-        # if this course existed before
-        if len(old_courses) > 0:
-            print('course has existed before')
-            old_course = old_courses[-1]
-            # if the old course's term does not match the new course's
-            if old_course.term != new_course.term:
-                print('adding because new term')
-                db.session.merge(new_course)
-            else:
-                db.session.delete(old_course)
-                print('replacing because same term')
-                db.session.add(new_course)
-        # if filter returns nothing, then this is a new course
+        
+        existing_course = Course.query.get((new_course.course_id, new_course.term))
+        print(existing_course)
+        if existing_course == None:    
+            # check for differences objects and then update 
+            check_differences(existing_course, new_course) 
         else:
-            # will throw sqlalchemy.exc.IntegrityError if course name 
-            # is incorrect but course_id is correct
-            try:
-                print('Course has not existed before')
-                db.session.add(new_course)
-            except:
-                print('course name is different but ID is the same')
+            db.session.add(existing_course)
         db.session.commit()
-
-def parse_and_store(path):
-    '''
-    Needs to run within an app_context!
-
-    :param path:
-    :return:
-    '''
-    data = json.load(open(path))
-    for datum in data:
-        course = Course(course_id=datum["Course"],
-                        call_number=datum["CallNumber"],
-                        course_name=datum["CourseTitle"],
-                        bulletin_flags=datum["BulletinFlags"],
-                        division_code=datum["DivisionCode"],
-                        class_notes=datum["ClassNotes"],
-                        num_enrolled=datum["NumEnrolled"],
-                        max_size=datum["MaxSize"],
-                        min_units=datum["MinUnits"],
-                        num_fixed_units=datum["NumFixedUnits"],
-                        term=datum["Term"],
-                        campus_name=datum["CampusName"],
-                        campus_code=datum["CampusCode"],
-                        school_code=datum["SchoolCode"],
-                        school_name=datum["SchoolName"],
-                        approval=datum["Approval"],
-                        prefix_name=datum["PrefixName"],
-                        prefix_long_name=datum["PrefixLongname"],
-                        instructor_name=datum["Instructor1Name"],
-                        type_name=datum["TypeName"],
-                        type_code=datum["TypeCode"]
-                        )
-
-        db.session.add(course)
-        db.session.commit()
+    print("database created and up to date!")
