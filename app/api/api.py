52cb1963854e14f24ce25ed39487104ec8a89184
Kevin Mao <32534083+kevin-mao@users.noreply.github.com>
2018-12-07 16:52:17 -0500

    Fixed CORS bug and added descriptions (#53)
    * recloned
    
    * work with me travis
    
    * implemented strict arguments and got rid of error help in app.config
    
    * playing around with get_courses, pulling to merge recent pull requests
    
    * started implementing db update checks
    
    * began adding update to helpers.py
    
    * added scheduler
    
    * added scheduler
    
    * adding check_differences() in helpers.py for updating db
    
    * oh i fixed it, i had to flush
    
    * fixed course parameters and added updating
    
    * flake8
    
    * travis my boy
    
    * tabs vs spaces
    
    * readme
    
    * added update.py (creates a new app and updates db with get_courses)
    
    * flake8 compliancy
    
    * removed test.py
    
    * fixed run.py, not sure why this didn't throw a bug yesterday
    
    * merged with bruk's changes
    
    * added students api and fixed order of student get method
    
    * fixed COR bug, fixed courses.txt, and added students.txt
    
    * hopefully flake8 compliant
    
    * removed pkg-resources from requirements.txt
    
    * removed those spaces

--- app/api/api.py
+++ app/api/api.py
@@ -108,8 +108,8 @@ class Courses(Resource):
                             404,
                             status=404,
                             message=f"Course with course_id = "
-                                    f"{args['course_id']} and term = "
-                                    f"{args['term']} does not exist")
+                            f"{args['course_id']} and term = "
+                            f"{args['term']} does not exist")
                     else:
                         datum['status'] = 200
                         datum['data'] = remove_hidden_attr(result.__dict__)
@@ -120,7 +120,7 @@ class Courses(Resource):
                     result = Course.query.filter_by(**args).all()
                     if not result:
                         abort(404, status=404,
-                              message=f'No courses with parameters {args}')
+                              message=f'No courses with {args}')
 
                     else:
                         datum['data'] = [
@@ -133,6 +133,103 @@ class Courses(Resource):
                   message="User couldn't be verified")
 
 
+class Students(Resource):
+    def process_args(args):
+        final_args = {}
+        del args['key']
+
+        for k in args.keys():
+            if args[k] is not None:
+                final_args[k] = args[k]
+
+        return final_args
+
+    def get(self, typ):
+        # select api requires the primary key: uni
+        if typ == 'select':
+            parser = reqparse.RequestParser()
+            # note that an error in passing in arguments here will not result
+            # in 400 error
+            parser.add_argument(
+                'uni',
+                required=True,
+                help="uni cannot be blank for select api")
+            parser.add_argument("key", required=True,
+                                help="key cannot be blank!")
+
+        # search api can accept any number and combination of prameters
+        # requires api key
+        elif typ == 'search':
+            parser = reqparse.RequestParser()
+            parser.add_argument('name')
+            parser.add_argument('uni')
+            parser.add_argument('email')
+            parser.add_argument('department')
+            parser.add_argument('title')
+            parser.add_argument('address')
+            parser.add_argument('home_address')
+            parser.add_argument('campus_tel')
+            parser.add_argument('tel')
+            parser.add_argument('fax')
+            parser.add_argument("key", required=True,
+                                help="key cannot be blank!")
+
+        else:
+            abort(
+                400,
+                status=400,
+                message=f"Bad Request. GET api/courses/select?"
+                f"or api/courses/search?")
+
+        args = parser.parse_args(strict=True)
+        key = args["key"]
+        datum = {}
+
+        # verify the API key
+        if User.verify(key):
+            # will return processed arguments, if there is an incorrect
+            # argument then it will return the first incorrect argument
+            args = Courses.process_args(args)
+            if isinstance(args, str):
+                abort(
+                    400,
+                    status=400,
+                    message=f"Invalid parameter: {args}")
+
+            elif isinstance(args, dict):
+                if typ == 'select':
+                    # if select api, then use get method by passing in primary
+                    # keys
+                    result = Course.query.get(args['uni'])
+                    if not result:
+                        abort(
+                            404,
+                            status=404,
+                            message=f"Student with uni = "
+                            f"{args['uni']} does not exist")
+                    else:
+                        datum['status'] = 200
+                        datum['data'] = remove_hidden_attr(result.__dict__)
+
+                elif typ == 'search':
+                    # for seach api, then use filter through db
+                    result = Course.query.filter_by(**args).all()
+                    if not result:
+                        abort(404, status=404,
+                              message=f'No students with {args}')
+
+                    else:
+                        datum['data'] = [
+                            remove_hidden_attr(
+                                i.__dict__) for i in result]
+                return jsonify(datum)
+        # if API key was not verified
+        else:
+            abort(403, status=403,
+                  message="User couldn't be verified")
+
+
 # api/courses/search dynamically searches by whatever parameters they input
 # api/courses/term/course_id passes a specific course
 api.add_resource(Courses, '/courses/<typ>')
+api.add_resource(Students, '/students/<typ>')
