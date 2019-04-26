42d4fa8ed7f7bf1242d4e31c0bafdd5e5daca839
Sharon Jin <sj2846@columbia.edu>
2019-04-26 13:40:04 -0400

    committing changes to residences

--- app/main/views.py
+++ app/main/views.py
@@ -1,5 +1,5 @@
 from flask import render_template, request
-from ..models import Course, Dining, Student
+from ..models import Course, Dining, Student, Residence
 from . import main
 import os
 from flask_cors import cross_origin
@@ -15,7 +15,9 @@ def search(term, where):
     # Very inefficient implementation
     # in the interest of "time" lol ironic
 
+    # FIX: for students.html, alld is empty list
     alld = where.query.all()
+    # print(alld)
     for one in alld:
         things = remove_hidden_attr(one.__dict__)
         for attr in things:
@@ -24,12 +26,16 @@ def search(term, where):
                 # from here, ifs become essential since
                 # returnables are very dependent on
                 # model type
-
                 if where == Course:
                     result.append({"course_id": things["course_id"],
                                    "term": things["term"],
                                    "name": things["course_name"]})
                 # more ifs based on models here
+                if where == Student:
+                    result.append({"Name": things["Name"],
+                                    "UNI": things["UNI"]})
+                if where == Residence:
+                    result.append({"Name": things["Name"]})
                 continue
 
     return result
@@ -65,9 +71,9 @@ def courses():
     search_results = []
     if request.form:
         term = request.form["searchTerm"]
-        print(term)
+        # print(term)
         search_results = search(term, Course)
-    print(search_results)
+    # print(search_results)
 
     return render_template(
         'main/courses.html',
@@ -76,17 +82,17 @@ def courses():
 
 
 # not implementable yet. Model hasn't been built
-@main.route('/housing', methods=['GET', 'POST'])
-def housing():
+@main.route('/residences', methods=['GET', 'POST'])
+def residences():
     search_results = []
     if request.form:
         term = request.form["searchTerm"]
-        search_results = search(term, "Housing")
+        search_results = search(term, Residence)
 
     return render_template(
-        'main/housing.html',
+        'main/residences.html',
         results=search_results,
-        parameters=get_parameters('housing.txt'))
+        parameters=get_parameters('residences.txt'))
 
 
 @main.route('/dining', methods=['GET', 'POST'])
@@ -106,7 +112,9 @@ def student():
 
     if request.form:
         term = request.form["searchTerm"]
+        print(term) # term = search query
         search_results = search(term, Student)
+    print(search_results)
 
     return render_template(
         'main/students.html',
